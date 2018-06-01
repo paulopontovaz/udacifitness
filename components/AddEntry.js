02b9e26569ed797f441e98e55e40f725b503f8b2
Paulo Vaz <paulopontovaz@gmail.com>
2018-06-01 07:12:31 -0300

    First commit

---
+++ components/AddEntry.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react'
+import { View, TouchableOpacity, Text } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
+import UdaciSlider from './UdaciSlider'
+import UdaciSteppers from './UdaciSteppers'
+import DateHeader from './DateHeader'
+
+
+function SubmitButton ({ onPress }) {
+	return (
+
+	)
+}
+
+export default class AddEntry extends Component {
+	state = {
+		run: 0,
+		bike: 0,
+		swim: 0,
+		sleep: 0,
+		eat: 0,
+	}
+
+	increment = metric => {
+		const { max, step } = getMetricMetaInfo(metric)
+		this.setState(state => {
+			const count = state[metric] + step
+
+			return {
+				...state,
+				[metric]: count > max ? max : count,
+			}
+		})
+	}
+
+	decrement = metric => {
+		const { step } = getMetricMetaInfo(metric)
+		this.setState(state => {
+			const count = state[metric] - step
+
+			return {
+				...state,
+				[metric]: count < 0 ? 0 : count,
+			}
+		})
+	}
+
+	slide = (metric, value) => {
+		this.setState( { [metric]: value } )
+	}
+
+	submit = () => {
+		const key = timeToString()
+		const entry = this.state
+
+		this.setState({
+			run: 0,
+			bike: 0,
+			swim: 0,
+			sleep: 0,
+			eat: 0,
+		})
+	}
+
+	render () {
+		const metaInfo = getMetricMetaInfo()
+
+		return (
+			<View>
+				<DateHeader date={(new Date()).toLocaleDateString()} />
+				{Object.keys(metaInfo).map(key => {
+					const { getIcon, type, ...otherProps } = metaInfo[key]
+					const value = this.state[key]
+
+					return (
+						<View key={key}>
+							{getIcon()}
+							{type === 'slider' ?
+								<UdaciSlider 
+									value={value} 
+									onChange={value => this.slide(key, value)} 
+									{...otherProps} /> 
+								:
+								<UdaciSteppers 
+									value={value} 
+									onIncrement={() => this.increment(key)} 
+									onDecrement={() => this.decrement(key)} 
+									{...otherProps} />
+							}
+						</View>
+					)
+				})}
+			</View>
+		)
+	}
+}
