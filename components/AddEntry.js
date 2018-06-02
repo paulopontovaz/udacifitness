c4d2ec5277e8fc673e9389c1bde2e61941f28610
Paulo Vaz <paulopontovaz@gmail.com>
2018-06-02 15:28:46 -0300

    Adding a lot of new stuff

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react'
-import { View, TouchableOpacity, Text } from 'react-native'
-import { getMetricMetaInfo, timeToString } from '../utils/helpers'
+import { View, TouchableOpacity, Text, Platform, StyleSheet } from 'react-native'
+import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
-
+import { Ionicons } from '@expo/vector-icons'
+import TextButton from './TextButton'
+import { submitEntry, removeEntry } from '../utils/api'
+import { connect } from 'react-redux'
+import { addEntry } from '../actions'
+import { white, purple } from '../utils/colors'
+import { NavigationActions } from 'react-navigation'
 
 function SubmitButton ({ onPress }) {
 	return (
-
+		<TouchableOpacity
+			style={Platform.OS === 'ios' ? styles.iosSubmitButton : styles.androidSubmitButton}
+			onPress={onPress}>
+				<Text style={styles.submitButtonText}>
+					SUBMIT
+				</Text>
+		</TouchableOpacity>
 	)
 }
 
-export default class AddEntry extends Component {
+class AddEntry extends Component {
 	state = {
 		run: 0,
 		bike: 0,
@@ -53,6 +65,10 @@ export default class AddEntry extends Component {
 		const key = timeToString()
 		const entry = this.state
 
+		this.props.dispatch(addEntry({
+			[key]: entry
+		}))
+
 		this.setState({
 			run: 0,
 			bike: 0,
@@ -60,20 +76,52 @@ export default class AddEntry extends Component {
 			sleep: 0,
 			eat: 0,
 		})
+
+		submitEntry({entry, key})
+		this.toHome()
+	}
+
+	reset = () => {
+		const key = timeToString()
+
+		this.props.dispatch(addEntry({
+			[key]: getDailyReminderValue()
+		}))
+
+		removeEntry(key)
+		this.toHome()
+	}
+
+	toHome = () => {
+		this.props.navigation.dispatch(NavigationActions.back({
+			key: 'AddEntry'
+		}))
 	}
 
 	render () {
 		const metaInfo = getMetricMetaInfo()
 
+		if (this.props.alreadyLogged) 
+			return (
+				<View style={styles.center}>
+					<Ionicons 
+						name={Platform.OS === "ios" ? "ios-happy-outline" : "md-happy"}
+						size={100}
+					/>
+					<Text>You already logged your information for today</Text>
+					<TextButton style={{padding: 10}} onPress={this.reset}>Reset</TextButton>
+				</View>
+			)
+
 		return (
-			<View>
+			<View style={styles.container}>
 				<DateHeader date={(new Date()).toLocaleDateString()} />
 				{Object.keys(metaInfo).map(key => {
 					const { getIcon, type, ...otherProps } = metaInfo[key]
 					const value = this.state[key]
 
 					return (
-						<View key={key}>
+						<View key={key} style={styles.row}>
 							{getIcon()}
 							{type === 'slider' ?
 								<UdaciSlider 
@@ -90,7 +138,61 @@ export default class AddEntry extends Component {
 						</View>
 					)
 				})}
+				<SubmitButton onPress={this.submit} />
 			</View>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 20,
+		backgroundColor: white,
+	},
+	row: {
+		flexDirection: 'row',
+		flex: 1,
+		alignItems: 'center',
+	},
+	iosSubmitButton: {
+		backgroundColor: purple,
+		padding: 10,
+		borderRadius: 7,
+		height: 45,
+		marginLeft: 40,
+		marginRight: 40,
+	},
+	androidSubmitButton: {
+		backgroundColor: purple,
+		padding: 10,
+		paddingLeft: 30,
+		paddingRight: 30,
+		height: 45,
+		borderRadius: 2,
+		alignSelf: 'flex-end',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	submitButtonText: {
+		color: white,
+		fontSize: 22,
+		textAlign: 'center',
+	},
+	center: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginRight: 30,
+		marginLeft: 30,
+	},
+})
+
+function mapStateToProps (state) {
+	const key = timeToString()
+	return {
+		alreadyLogged: state[key] && !state[key].today
+	}
+}
+
+export default connect(mapStateToProps)(AddEntry)
--- components/UdaciSteppers.js
+++ components/UdaciSteppers.js
@@ -1,10 +1,71 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Platform } from 'react-native'
+import { FontAwesome, Entypo } from '@expo/vector-icons'
+import { white, gray, purple } from '../utils/colors'
 
-export default function UdaciSteppers () {
+export default function UdaciSteppers ({max, unit, step, onIncrement, onDecrement, value}) {
 	return (
-		<View>
-			<Text>UdaciSteppers</Text>
+		<View style={[styles.row, {justifyContent: 'space-between'}]}>
+			{Platform.OS === 'ios' ? 
+				<View style={{flexDirection: 'row'}}>
+					<TouchableOpacity 
+						style={[styles.iosButton, {borderTopRightRadius: 0, borderBottomRightRadius: 0}]}
+						onPress={onDecrement}>
+							<Entypo name="minus" size={30} color={purple} />
+					</TouchableOpacity>
+					<TouchableOpacity 
+						style={[styles.iosButton, {borderTopLeftRadius: 0, borderBottomLeftRadius: 0}]}
+						onPress={onIncrement}>
+							<Entypo name="plus" size={30} color={purple} />
+					</TouchableOpacity>
+				</View>
+				:
+				<View style={{flexDirection: 'row'}}>
+					<TouchableOpacity 
+						style={[styles.androidButton]}
+						onPress={onDecrement}>
+							<FontAwesome name="minus" size={30} color={white} />
+					</TouchableOpacity>
+					<TouchableOpacity 
+						style={[styles.androidButton]}
+						onPress={onIncrement}>
+							<FontAwesome name="plus" size={30} color={white} />
+					</TouchableOpacity>
+				</View>
+			}
+			
+			<View style={styles.metricCounter}>
+				<Text style={{fontSize: 24, textAlign: 'center'}}>{value}</Text>
+				<Text style={{fontSize: 18, color: gray}}>{unit}</Text>
+			</View>			
 		</View>
 	)		
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+  },
+  androidButton: {
+  	margin: 5,
+  	 backgroundColor: purple,
+  	 padding: 10,
+  	 borderRadius: 2,
+  },
+  iosButton: {
+    backgroundColor: white,
+    borderColor: purple,
+    borderWidth: 1,
+    borderRadius: 3,
+    padding: 5,
+    paddingLeft: 25,
+    paddingRight: 25,
+  },
+  metricCounter: {
+  	width: 85,
+  	justifyContent: 'center',
+  	alignItems: 'center',
+  },
+})
